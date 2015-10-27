6106496bd2666450022833d70b2f9ba04bfbea7f
rcharron <raphael.charrondiere@ens-lyon.fr>
2015-10-27 19:26:05 +0100

    repport

--- Project1/Raphaël/launchscript.m
+++ Project1/Raphaël/launchscript.m
@@ -6,7 +6,7 @@ Data=csvread('../train.csv');
 [X,Y,Xv,Yv,fraw]=treatData(Data);
 
 [fb,betaB]=linearbayesian(X,Y);
-[fridge,betaRidge]=ridge(X,Y,1/2);
+[fridge,betaRidge]=ridge(X,Y,10);
 [flasso,betaLasso]=linearlasso(X,Y,1/3);
 
 [errb]=empiricalError(X,Y,fb);
@@ -19,8 +19,8 @@ Data=csvread('../train.csv');
 %%
 %%Manytest for a method
 [fb,Betab,errb]= manytest(Data,@linearbayesian,1000);
-[flasso,Betalasso,errlasso]= manytest(Data,@(X,Y)linearlasso(X,Y,1/3),100);
-[fridge,Betaridge,errridge]= manytest(Data,@(X,Y)ridge(X,Y,1/2),1000);
+[flasso,Betalasso,errlasso]= manytest(Data,@(X,Y)linearlasso(X,Y,1/20),100);
+[fridge,Betaridge,errridge]= manytest(Data,@(X,Y)ridge(X,Y,10),1000);
 
 %%
 %%Predict
--- Project1/Raphaël/makefeatures.m
+++ Project1/Raphaël/makefeatures.m
@@ -1,3 +1,14 @@
-function [X]= makefeatures(X)
-    X=[X;(log(X))];
+function [res]= makefeatures(X)
+    n=size(X,2);
+    res=zeros(size(makefeaturesrow(X(:,1)),1),n);
+    for i=1:n
+        res(:,i)=makefeaturesrow(X(:,i));
+    end
+%    X=[X;(log(X));1./X];
+end
+
+function [X]= makefeaturesrow(X)
+Y=X*X';
+Inv=1./X;
+X=[X;(log(X));Inv;Y(:)];
 end
